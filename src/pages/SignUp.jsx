6ee376482d32637d49b025f2453f258f2a54dff4
vova <bordiian.v@gmail.com>
2022-12-24 15:25:01 +0300

    added firebase authentication by email/password

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -19,8 +21,22 @@ function SignUp() {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
-    
+    })) 
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
   }
 
     return (
@@ -30,7 +46,7 @@ function SignUp() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input type="text" className="nameInput" 
           placeholder='Name' id='name' value={name} onChange={onChange} />
           <input type="email" className="emailInput" 
