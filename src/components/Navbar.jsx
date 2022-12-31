87661b2810f50ca2ade24e669ee0df5b89bd0d8b
Vova <bordiian.v@gmail.com>
2022-12-31 17:53:29 +0200

    created offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useLocation } from "react-router-dom"
-import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
-import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
-import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
 
 
 function Navbar() {
@@ -24,7 +24,7 @@ function Navbar() {
           </li>
           <li className="navbarListItem" onClick={() => navigate('/offers')}>
             <OfferIcon fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
-            <p className={pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offer</p>
+            <p className={pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offers</p>
           </li>
           <li className="navbarListItem" onClick={() => navigate('/profile')}>
             <PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
