01cee926dbc6207d3efabe6b1e62ac17887572dc
vova <bordiian.v@gmail.com>
2022-12-31 14:09:04 +0300

    Not done with fetching listings

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { collection, getDocs, query, orderBy, limit, where, startAfter } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { toast } from "react-toastify"
+import Spinner from "../components/Spinner"
+
 
 function Category() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+
+    }
+    fetchListings()
+  }, [])
+
   return (
     <div>Category</div>
   )
